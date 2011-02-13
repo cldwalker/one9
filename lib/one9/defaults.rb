0e7835baf0d2a2cebf5134f5828892d445c6a58a
Gabriel Horner <gabriel.horner@gmail.com>
2011-02-13 15:10:52 -0500

    disable String#each for Array false positives

--- lib/one9/defaults.rb
+++ lib/one9/defaults.rb
@@ -10,7 +10,6 @@ change 'Date.parse', 'mm/dd/yyyy syntax does not exist anymore'
 change 'Time.parse', 'mm/dd/yyyy syntax does not exist anymore'
 change 'Proc#arity', 'Number of parameters that would not be ignored instead of ...'
 delete 'Array#choice', 'Use Array#sample instead'
-delete 'String#each', 'Use String#each_line instead'
 delete 'Kernel#to_a', 'Replace with Array()'
 delete 'Object#type', 'Replace with #class'
 delete 'Array#nitems', "Replace with #compact.size"
@@ -21,9 +20,11 @@ delete 'Array#indices', 'Replace with #values_at'
 delete 'Exception#to_str', 'Replace with #to_s'
 
 # * BUGGY *
-# doesn't work when called in a preload env
+# doesn't work when preloaded ('-r') in rails
 # change 'Object#=~', 'Returns nil instead of false'
 # causes memory leaks
 # change 'String#[]', 'Returns string instead of number'
-# catches extra calls i.e. Array()
+#
+# too many false positives with Array()
 # delete 'String#to_a', 'Replace with Array()'
+# delete 'String#each', 'Use String#each_line instead'
